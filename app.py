b45aa962921d0df83e978b26f18f6172fbb83dfc
diogoaq1 <diogo_quintiliano@hotmail.com>
2025-08-29 16:12:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def gerar_qrcode_chave_nfe(chave_nfe):
 
 # 🚀 Inicialização do Flask
 load_dotenv()
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or secrets.token_hex(32)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_BINDS'] = {
